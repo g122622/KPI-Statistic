d43d37ca9dc460681a750b444cbb51d5cf24500b
g122622 <3300925806@qq.com>
2020-03-04 00:09:14 +0800

    功能更新

--- KPI-Statistic.cpp
+++ KPI-Statistic.cpp
@@ -26,11 +26,16 @@ int digitHour(int& num)
 void main()
 {
 	vector<int> temp;
-	int count = 0;
+	int count = 0, sleep_duration, awake_time;
 	float kpi = 0.0;
 	cout << "1.输入四位整数（或者三位）。" << endl
 		<< "2.按照时间格式。" << endl
 		<< "3.输入0停止采集数据，开始计算。" << endl;
+	// 开始输入KPI各项数据
+	cout << "[起床时刻]" << endl;
+	cin >> awake_time;
+	cout << "[睡眠时间（区间）]" << endl;
+	cin >> sleep_duration;
 	while (true)
 	{
 		count++;
@@ -44,9 +49,12 @@ void main()
 		}
 	}
 
-	// start to calculate KPI
+	// 开始计算KPI
 	for (auto item : temp)
 		kpi += (float)(24.0 - (digitMin(item) / 60.0) - digitHour(item)) * 20.0;
+	kpi += (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time)) * 100.0;
+	kpi += (float)((digitMin(sleep_duration) / 60.0) + digitHour(sleep_duration)) * 100.0 - 500.0;
 	cout << "KPI为：" << kpi << endl;
+	cout << "睡眠指数：" << (float)((digitMin(sleep_duration) / 60.0) + digitHour(sleep_duration)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time));
 	system("pause");
 }

ad72643219eda71e110e35b21cebba58d4bcd77d
g122622 <3300925806@qq.com>
2020-03-25 09:10:07 +0800

    nothing.

--- KPI-Statistic.cpp
+++ KPI-Statistic.cpp
@@ -2,32 +2,60 @@
 #include <vector>
 using namespace std;
 
-int digitMin(int& num)
+// 全局变量
+vector<int> temp;
+int sleep_duration = 0, awake_time = 0;
+float kpi = 0.0;
+
+int digitMin(int num)
 {
-	int tmp = num;	// make it a var
 	int result;		// storage result
-	result = tmp % 10;
-	tmp /= 10;
-	result += (tmp % 10) * 10;
+	result = num % 10;
+	num /= 10;
+	result += (num % 10) * 10;
 	return result;
 }
 
-int digitHour(int& num)
+int digitHour(int num)
 {
-	int tmp = num;	// make it a var
 	int result;		// storage result
-	tmp /= 100;
-	result = tmp % 10;
-	tmp /= 10;
-	result += (tmp % 10) * 10;
+	num /= 100;
+	result = num % 10;
+	num /= 10;
+	result += (num % 10) * 10;
 	return result;
 }
 
+float digitTime(int num)
+{
+	return (float)((digitMin(num) / 60.0) + digitHour(num));
+}
+
+class action
+{
+public:
+	void calcKPI()
+	{
+		for (auto item : temp)
+			kpi += (float)(24.0 - digitTime(item)) * 20.0;
+		kpi += (float)(9.0 - digitTime(awake_time)) * 100.0;
+		kpi += (float)(digitTime(sleep_duration)) * 100.0 - 500.0;
+		cout << "KPI为：" << kpi << endl;
+		kpi = 0.0;
+		return;
+	}
+
+	void calcSI()
+	{
+		cout << "睡眠指数：" << (float)digitTime(sleep_duration) * \
+			(float)(9.0 - digitTime(awake_time)) * \
+			(float)(9.0 - digitTime(awake_time)) << endl;
+		return;
+	}
+};
 void main()
 {
-	vector<int> temp;
-	int count = 0, sleep_duration, awake_time;
-	float kpi = 0.0;
+	action action;
 	cout << "1.输入四位整数（或者三位）。" << endl
 		<< "2.按照时间格式。" << endl
 		<< "3.输入0停止采集数据，开始计算。" << endl;
@@ -36,8 +64,11 @@ void main()
 	cin >> awake_time;
 	cout << "[睡眠时间（区间）]" << endl;
 	cin >> sleep_duration;
-	cout << "睡眠指数：" << (float)((digitMin(sleep_duration) / 60.0) + digitHour(sleep_duration)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time));
-	cout << endl;
+	action.calcSI();
+	action.calcKPI();
+	cout << endl << endl;
+
+	int count = 0;
 	while (true)
 	{
 		count++;
@@ -49,15 +80,12 @@ void main()
 			temp.erase(temp.end() - 1);
 			break;
 		}
+		action.calcKPI();
 	}
 
+	cout << endl << endl << "==========" << endl;
 	// 开始计算KPI
-	for (auto item : temp)
-		kpi += (float)(24.0 - (digitMin(item) / 60.0) - digitHour(item)) * 20.0;
-	kpi += (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time)) * 100.0;
-	kpi += (float)((digitMin(sleep_duration) / 60.0) + digitHour(sleep_duration)) * 100.0 - 500.0;
-	cout << "KPI为：" << kpi << endl;
-	cout << "睡眠指数：" << (float)((digitMin(sleep_duration) / 60.0) + digitHour(sleep_duration)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time)) * (float)(9.0 - (digitMin(awake_time) / 60.0) - digitHour(awake_time));
-	cout << endl;
+	action.calcKPI();
+	action.calcSI();
 	system("pause");
 }

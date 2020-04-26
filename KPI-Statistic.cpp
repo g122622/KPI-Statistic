ef52cba7f3c2991483781db6eac8236795f1d506
g122622 <3300925806@qq.com>
2020-04-26 23:09:34 +0800

    nothing.

--- KPI-Statistic.cpp
+++ KPI-Statistic.cpp
@@ -3,14 +3,10 @@
 //#include "gymath.h"
 using namespace std;
 
-// 全局变量
-vector<int> temp;
-int sleep_time = 0, set_out_time = 0, shcool_homework_finish_time = 0;
-float kpi = 0.0;
-
+/*----------函数----------*/
 int digitMin(int num)
 {
-	int result;		// storage result
+	int result;
 	result = num % 10;
 	num /= 10;
 	result += (num % 10) * 10;
@@ -19,7 +15,7 @@ int digitMin(int num)
 
 int digitHour(int num)
 {
-	int result;		// storage result
+	int result;
 	num /= 100;
 	result = num % 10;
 	num /= 10;
@@ -32,16 +28,26 @@ float digitTime(int num)
 	return (float)((digitMin(num) / 60.0) + digitHour(num));
 }
 
+/*----------全局变量----------*/
+vector<int> temp;
+int sleep_time = 0,
+set_out_time = 0,
+shcool_homework_finish_time = 0,
+lg_times = 0;
+float kpi = 0.0;
+
+/*----------对象----------*/
 class action
 {
 public:
 	void calcKPI()
 	{
 		for (auto item : temp)
-			kpi += (float)(24.0 - digitTime(item)) * 20.0;
+			kpi += (float)(24.0 - digitTime(item)) * 500.0;
 		kpi += (float)(7.5 - digitTime(set_out_time)) * 5000.0;
 		kpi += (float)(24.0 - digitTime(sleep_time)) * 5000.0;
 		kpi += (float)(24.0 - digitTime(shcool_homework_finish_time)) * 500.0;
+		kpi += (float)(2 - lg_times) * 300.0;
 		cout << "KPI为：" << kpi << endl;
 		kpi = 0.0;
 		return;
@@ -55,6 +61,8 @@ public:
 		return;
 	}*/
 };
+
+/*----------主函数----------*/
 void main()
 {
 	action Action;
@@ -68,8 +76,8 @@ void main()
 	cin >> sleep_time;
 	cout << "[完成学校作业时间]" << endl;
 	cin >> shcool_homework_finish_time;
-//	cout << "[***]" << endl;
-//	cin >> ***;
+	cout << "[次数]" << endl;
+	cin >> lg_times;
 
 //	Action.calcSI();
 //	Action.calcKPI();
